ad0e023e01f79774788ecb32e751aaf4baaa2c13
vkazbanov <vkazbanov@telematika.com>
2023-12-18 16:55:19 +0200

    Auto-generate packet handler trait

--- src/macros.rs
+++ src/macros.rs
@@ -17,12 +17,26 @@ macro_rules! packet {
 
         paste::paste! {
             impl $ty_name {
+                $vis fn handle_by<H>(self, handler: &mut H) where H: [<$ty_name Handler>] {
+                    match self {
+                        $(
+                            Self :: $name($name) => handler.[<handle_ $name:snake>] ($name)
+                        ),*
+                    }
+                }
+
                 $(
                     $vis fn [<$name:snake>] ($($field : $ty),*) -> Self {
                         Self :: $name($name {
                             $($field),*
                         })
                     }
+                )*
+            }
+
+            $vis trait [<$ty_name Handler>] {
+                $(
+                    fn [<handle_ $name:snake>] (&mut self, packet: $name);
                 )*
             }
         }
